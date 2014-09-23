a915b8c01f58daf14687dc738aa8dee83c072fc8
shihabahmed <shihab.bhuiyan@gmail.com>
2014-09-23 14:57:45 +0600

    Align elements vertically or horizontally

---
+++ README.md
@@ -0,0 +1,15 @@
+jquery.align.js
+===============
+This plugin aligns elements vertically/horizontally within the parent element.
+
+Dependency
+----------
+1. jQuery
+
+Usage
+-----
+	<div>
+		<p data-valign="middle">Vertically middle</p>
+	</div>
+
+Alignment is done once the window has finished loading. For ajax requests, call <code>align()</code> on success.
---
+++ jquery.align.js
@@ -0,0 +1,76 @@
+﻿
+/**
+* jquery.align.js
+* Version: 1.1
+* Requires: jQuery 1.4+
+*
+* Created by Shihab
+*/
+
+; (function ($) {
+	$.fn.extend({
+
+		vAlign: function (alignment) {
+			return this.each(function () {
+				var vAlign_parent, top, elem = $(this);
+				vAlign_parent = elem.parent();
+
+				if (alignment.toLowerCase() == "middle") {
+					top = ($(vAlign_parent).height() - elem.outerHeight()) / 2;
+					top = top < 0 ? 0 : top;
+				} else if (alignment.toLowerCase() == "bottom") {
+					top = $(vAlign_parent).height() - elem.outerHeight();
+				} else {
+					top = 0;
+				}
+
+				elem.css("position", "relative").css({
+					"top": top
+				}, 200);
+			});
+		}, // vAlign
+
+		align: function (alignment) {
+			return this.each(function () {
+				var align_parent, left, elem = $(this);
+				align_parent = elem.parent();
+
+				if (alignment.toLowerCase() == "center") {
+					left = ($(align_parent).width() - elem.outerWidth()) / 2;
+					left = left < 0 ? 0 : left;
+				} else if (alignment.toLowerCase() == "right") {
+					left = $(align_parent).width() - elem.outerWidth();
+				} else {
+					left = 0;
+				}
+
+				elem.css("position", "relative").css({
+					"left": left
+				}, 200);
+			});
+		} // align
+	});
+
+	$(function () {
+		if ($('[data-valign]').length > 0) {
+			$('[data-valign]').each(function () {
+				var elem = $(this);
+				elem.vAlign(elem.attr("data-valign"));
+
+				$(window).resize(function () {
+					elem.vAlign(elem.attr("data-valign"));
+				});
+			});
+		}
+		if ($('[data-align]').length > 0) {
+			$('[data-align]').each(function () {
+				var elem = $(this);
+				elem.align(elem.attr("data-align"));
+
+				$(window).resize(function () {
+					elem.align(elem.attr("data-align"));
+				});
+			});
+		}
+	});
+})(jQuery);
